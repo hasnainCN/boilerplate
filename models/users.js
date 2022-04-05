97a9830d6af742565cb1bd8926484f583ca0d3d1
hasnainCN <muhammad.hassnain@codeninjaconsulting.com>
2022-04-05 15:02:45 +0500

    server ruuning

--- models/users.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema; // <-- EDIT: missing in the original post
-var UsersSchema = new Schema({
-  name: String,
-  email: String,
-  date: Date,
-});
-var Users = mongoose.model('Users', UsersSchema);
-module.exports = Users;
